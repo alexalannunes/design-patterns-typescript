88ec81d2dfe3e10a9705db75324cb763998f5f17
Alex Alan Nunes <alexalannunes@gmail.com>
2023-01-27 11:16:04 -0300

    doc: add comment with result

--- src/creational/singleton/index.ts
+++ src/creational/singleton/index.ts
@@ -1,5 +1,13 @@
 import { dbA } from "./db-a";
 import { dbB } from "./db-b";
 
-dbB.show();
+// const dbA = Database.getInstance();
+// dbA.add({ name: "alex", age: 24 });
+// dbA.add({ name: "alan", age: 24 });
+
+// const dbB = Database.getInstance();
+// dbB.add({ name: "nunes", age: 24 });
+// dbB.add({ name: "lima", age: 24 });
+
+dbB.show(); // all users
 console.log(dbA === dbB);
