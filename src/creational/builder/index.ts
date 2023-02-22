4baa9b1dd2cf8238605997a1f34bc33781d3d62d
Alex Alan Nunes <alexalannunes@gmail.com>
2023-02-22 10:50:04 -0300

    feat: vegan builder

--- src/creational/builder/index.ts
+++ src/creational/builder/index.ts
@@ -1,5 +1,11 @@
 import { MainDishBuilder } from "./classes/main-dish-builder";
+import { VeganDishBuilder } from "./classes/vegan-dish-builder";
 
 const mainDishBuilder = new MainDishBuilder();
 mainDishBuilder.makeMeal().makeDessert();
 console.log(mainDishBuilder.getMeal());
+
+const veganDishBuilder = new VeganDishBuilder();
+
+const vegan = veganDishBuilder.makeMeal();
+console.log(vegan.getMeal(), vegan.getMeal().getPrice());
